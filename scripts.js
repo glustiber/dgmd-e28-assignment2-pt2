3b7abaadbdcf1bf281f797075a414045ce74c9fe
Gregory Lustiber <glustiber@gmail.com>
2023-02-19 16:30:06 -0500

    place x or o on board

--- scripts.js
+++ scripts.js
@@ -1,13 +1,30 @@
 $(document).ready(function(){
 
-    let turn = "O";
+    let turn = "X";
 
-    $('.turn').html(turn);
+    $(".turn").html(turn);
 
+    // generate board
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
             $(".board").append( $("<div class='square'></div>") );
         }
     }
+
+    // place X or O on board
+    $(".square").click(function() {
+        if( $(this).is(":empty") ) {
+            if (turn === "X") {
+                $(this).html("<div class='x'>X</div>");
+                turn = "O";
+                $(".turn").html(turn);
+            }
+            else if (turn === "O") {
+                $(this).html("<div class='o'>O</div>");
+                turn = "X";
+                $(".turn").html(turn);
+            }
+        }
+    });
 
 });
